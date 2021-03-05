7d98abc9046a0c26d3a35f6acd41755b866d581d
morgju08 <79654259+morgju08@users.noreply.github.com>
2021-03-05 17:39:14 -0500

    Create sql_playground.py

---
+++ sql_playground.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def main():
+    print("Hello world!")
+    conn = sqlite3.connect('tweet_db.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Tweets ORDER BY Likes DESC")
+    results = cursor.fetchall()
+    print(results[0])
+    for x in results:
+        print("TWEET: %s  LIKES: %d" % (x[0], x[2]))
+
+main()
