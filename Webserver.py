5d892c128f2279f50ae920a5de1e988c32624f34
morgju08 <79654259+morgju08@users.noreply.github.com>
2021-03-15 13:12:44 -0400

    git commit

--- Webserver.py
+++ Webserver.py
@@ -5,6 +5,7 @@ import aiohttp
 import aiohttp_jinja2
 import jinja2
 import sqlite3
+import requests
 
 
 @aiohttp_jinja2.template('Homepage.html.jinja2')
@@ -44,12 +45,16 @@ async def safety(request):
 async def add_tweet(request):
     data = await request.post()
     content = data['content']
+
+    target = request.remote
+    print("User is coming from %s" % target)
+    location = get_location(target)
+    print("User is at: %s " % location)
+
     # INSERT INTO Tweets(content,likes) VALUES ("new tweet!",0);
-    query = "INSERT INTO Tweets (content, likes) VALUES (\"%s\",0)" % content
-    print("Query:  %s" % query)
     conn = sqlite3.connect('tweet_db.db')
     cursor = conn.cursor()
-    cursor.execute(query)
+    cursor.execute("INSERT INTO Tweets (content,likes,location) VALUES (?,0,?)", (content,location))
     conn.commit()
     print("The user tweeted:  %s" % data['content'])
     raise web.HTTPFound('/Classes')
@@ -58,13 +63,33 @@ async def like(request):
     conn = sqlite3.connect('tweet_db.db')
     cursor = conn.cursor()
     tweet_id = request.query['id']
-    cursor.execute("SELECT Likes FROM Tweets WHERE id=%s" % tweet_id)
+    query = "SELECT Likes FROM Tweets WHERE id=?", (tweet_id,)
+    print("Query:  %s" % query)
+    cursor.execute(query)
     like_count = cursor.fetchone()[0]
     # add 1 to the like count and save it
-    cursor.execute("UPDATE Tweets SET Likes=%d WHERE id=%s" % (like_count + 1, tweet_id))
+    cursor.execute("UPDATE Tweets SET Likes=? WHERE id=?", (like_count + 1, tweet_id))
     conn.commit()
     conn.close()
     raise web.HTTPFound('/Classes')
+
+async def like_json(request):
+    conn = sqlite3.connect('tweet_db.db')
+    cursor = conn.cursor()
+    tweet_id = request.query['id']
+    cursor.execute("SELECT Likes FROM Tweets WHERE id=?", (tweet_id,))
+    like_count = cursor.fetchone()[0]
+    # add 1 to the like count and save it
+    cursor.execute("UPDATE Tweets SET Likes=? WHERE id=?", (like_count + 1, tweet_id))
+    conn.commit()
+    conn.close()
+    return web.json_response(data={"like_count":like_count})
+
+def get_location(target):
+    api_key = "15e4c318fe79628787ed5571ea69f63f"
+    result = requests.get("http://api.ipstack.com/%s?access_key=%s" % (target, api_key))
+    ip_address_info = result.json()
+    return ip_address_info["city"]
 
 
 
@@ -82,10 +107,11 @@ def main():
                     web.get('/Safety', safety),
                     web.static('/static','static',show_index=False),
                     web.post('/tweet', add_tweet),
-                    web.get('/like',like)])
+                    web.get('/like',like),
+                    web.get('/likes.json',like_json)])
 
     print("Hi!!! Welcome to Webserver 1.0")
-    web.run_app(app, host="0.0.0.0",port=80)
+    web.run_app(app, host="127.0.0.1",port=3000)
 
 
 
